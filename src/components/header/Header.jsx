fe812d790829fc6e77899df3df76d79a44919e62
Hernán Pablo Lijó <lijo.hernanpablo@gmail.com>
2023-11-13 20:28:55 -0300

    correccion del Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import './header.css'
-import Navbar from "../navBar/NavBar";
+import NavBar from "../navBar/NavBar";
 import logo from "/src/assets/logo.jpg"
-import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header className="header">
                 <a href="/" className="header__cont"><img src= {logo} alt="logo Ecomerce" className="header__img"/></a>
                 <h1 className="header__title">Insumos React</h1>
-                <Navbar/>
+                <NavBar/>
         </header>
     )
 }
